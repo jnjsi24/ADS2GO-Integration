dcb7a618eb8b897530d5f2b68cc1ce81069ba794
Carl Glean <carljustineglean@gmail.com>
2025-08-23 15:10:32 +0800

    some files for the androidapp

--- Ads2Go-MobileApp/src/graphql/apolloClient.ts
+++ Ads2Go-MobileApp/src/graphql/apolloClient.ts
@@ -1,8 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://192.168.100.22:5000/graphql', // Your PC's IP address
+});
 
 const client = new ApolloClient({
-  uri: "http://192.168.100.5:5000/graphql", // 👈 use your actual local IP
+  link: httpLink,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'cache-and-network',
+      errorPolicy: 'all',
+    },
+    query: {
+      errorPolicy: 'all',
+    },
+    mutate: {
+      errorPolicy: 'all',
+    },
+  },
 });
 
 export default client;
