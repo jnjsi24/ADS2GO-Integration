c08c31ec3ce75797af893b83cd9d1a888da3f32c
EMBOT1DO <nicofaith40@gmail.com>
2025-10-08 12:03:26 +0800

    analytics

--- Ads2Go-DriverMobileApp/config/api.ts
+++ Ads2Go-DriverMobileApp/config/api.ts
@@ -14,7 +14,7 @@ if (!EXPO_PUBLIC_API_URL && (!serverIp || !serverPort)) {
   throw new Error('Missing required environment variables for API configuration');
 }
 
-const serverUrl = serverIp && serverPort ? `http://192.168.100.2:8081` : null;
+const serverUrl = serverIp && serverPort ? `http://192.168.1.92:8081` : null;
 
 
 const API_CONFIG = {
