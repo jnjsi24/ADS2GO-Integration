93fcff24ffa298d2e5e63db9320df779581d007a
Carl Glean <carljustineglean@gmail.com>
2025-10-13 20:32:24 +0800

    errors

--- Ads2Go-Client/src/pages/ADMIN/AdminAdsControl.tsx
+++ Ads2Go-Client/src/pages/ADMIN/AdminAdsControl.tsx
@@ -330,9 +330,17 @@ const AdminAdsControl: React.FC = () => {
               timestamp: update.timestamp
             });
             
-            const updatedScreen = {
+            const updatedScreen: ScreenData = {
               ...screen,
               screenMetrics: {
+                isDisplaying: screen.screenMetrics?.isDisplaying ?? true,
+                brightness: screen.screenMetrics?.brightness ?? 100,
+                volume: screen.screenMetrics?.volume ?? 100,
+                adPlayCount: screen.screenMetrics?.adPlayCount ?? 0,
+                maintenanceMode: screen.screenMetrics?.maintenanceMode ?? false,
+                displayHours: screen.screenMetrics?.displayHours ?? 0,
+                adPerformance: screen.screenMetrics?.adPerformance ?? [],
+                lastAdPlayed: screen.screenMetrics?.lastAdPlayed ?? '',
                 ...screen.screenMetrics,
                 currentAd: {
                   adId: update.adId,
@@ -366,31 +374,31 @@ const AdminAdsControl: React.FC = () => {
       setActionLoading(action);
       let result;
       
-      // Use GraphQL mutations for bulk actions
+      // Use API service for bulk actions
       switch (action) {
         case 'sync':
-          result = await graphQLService.syncAllScreens();
+          result = await apiService.syncAllScreens();
           break;
         case 'play':
-          result = await graphQLService.playAllScreens();
+          result = await apiService.playAllScreens();
           break;
         case 'pause':
-          result = await graphQLService.pauseAllScreens();
+          result = await apiService.pauseAllScreens();
           break;
         case 'stop':
-          result = await graphQLService.stopAllScreens();
+          result = await apiService.stopAllScreens();
           break;
         case 'restart':
-          result = await graphQLService.restartAllScreens();
+          result = await apiService.restartAllScreens();
           break;
         case 'emergency':
-          result = await graphQLService.emergencyStopAll();
+          result = await apiService.emergencyStopAll();
           break;
         case 'lockdown':
-          result = await graphQLService.lockdownAllScreens();
+          result = await apiService.lockdownAllScreens();
           break;
         case 'unlock':
-          result = await graphQLService.unlockAllScreens();
+          result = await apiService.unlockAllScreens();
           break;
         default:
           throw new Error('Unknown action');
@@ -414,22 +422,22 @@ const AdminAdsControl: React.FC = () => {
       
       switch (action) {
         case 'metrics':
-          result = await graphQLService.updateScreenMetrics(deviceId, value);
+          result = await apiService.updateScreenMetrics(deviceId, value);
           break;
         case 'start-session':
-          result = await graphQLService.startScreenSession(deviceId);
+          result = await apiService.startScreenSession(deviceId);
           break;
         case 'end-session':
-          result = await graphQLService.endScreenSession(deviceId);
+          result = await apiService.endScreenSession(deviceId);
           break;
         case 'track-ad':
-          result = await graphQLService.trackAdPlayback(deviceId, value.adId, value.adTitle, value.adDuration);
+          result = await apiService.trackAdPlayback(deviceId, value.adId, value.adTitle, value.adDuration);
           break;
         case 'end-ad':
-          result = await graphQLService.endAdPlayback(deviceId);
+          result = await apiService.endAdPlayback(deviceId);
           break;
         case 'driver-activity':
-          result = await graphQLService.updateDriverActivity(deviceId, value);
+          result = await apiService.updateDriverActivity(deviceId, value);
           break;
         default:
           throw new Error('Unknown action');
